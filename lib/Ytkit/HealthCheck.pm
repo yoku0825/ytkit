fa0636a602fbb9505118ffe68c6098765bd7af51
yoku0825 <yoku0825@gmail.com>
2018-06-19 18:06:11 +0900

    Display user@host when long query detected.

--- lib/Ytkit/HealthCheck.pm
+++ lib/Ytkit/HealthCheck.pm
@@ -270,7 +270,8 @@ sub check_long_query
     my $time= $row->{Time} ? $row->{Time} : 0;
     my $status= compare_threshold($time, $self->{long_query});
 
-    $self->update_status($status, sprintf(qq{Detected long query: "%s"\t}, $row->{Info})) if $status && $row->{Info};
+    $self->update_status($status, sprintf(q{Detected long query by %s@%s: "%s"},
+                                          $row->{User}, $row->{Host}, $row->{Info})) if $status && $row->{Info};
 
   } ### End of foreach, goes to the next row.
 

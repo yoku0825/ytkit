530e9f6776419c0e70b8efc95d1b6ac6ad2b5387
yoku0825 <yoku0825@gmail.com>
2021-01-19 11:55:25 +0900

    Handle version warning for decide_role

--- lib/Ytkit/HealthCheck.pm
+++ lib/Ytkit/HealthCheck.pm
@@ -221,7 +221,15 @@ sub decide_role
 
   $master= 1 if $self->show_slaves_via_processlist;
   $slave = 1 if ($self->instance->show_slave_status && $self->instance->show_slave_status->[0]);
+
+  ### Stop _carp within internal version handling
+  my $saved_ignore= $self->{_ignore_unsupport_version};
+  $self->{_ignore_unsupport_version}= 1;
+
   $cluster= 1 if defined($self->instance->i_am_group_replication_primary);
+  
+  ### Restore param
+  $self->{_ignore_unsupport_version}= $saved_ignore;
 
   ### SHOW SLAVE STATUS condition is advanced more than cluster.
   if ($slave)
